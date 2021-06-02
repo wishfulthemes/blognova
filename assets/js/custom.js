53febc415faa7db3d3b7737fa6188573db76ac98
Ashis Bajracharya <ashis.n1tech@gmail.com>
2021-06-03 00:42:29 +0545

    keyboard focus login completion on 2 level menu

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -7,44 +7,7 @@
     $(this).siblings(".sub-menu").toggleClass("show")
     // console.log("this function works")
   })
-  // //show submenu on enter keypress
-  $(".menu-item-has-children .submenu-icon").on("keypress", function (e) {
-    console.log("this functionworks on keypress")
-    var keycode = e.keyCode ? e.keyCode : e.which
-    if (keycode == "13") {
-      $(this).siblings(".sub-menu").toggleClass("show")
-    }
-  })
-  // //hide submenu on reverse selection
-  $(".menu-item-has-children a").on("focusin", function () {
-    $(this).siblings(".sub-menu").removeClass("show")
-    // var menuItem = $(this).parent().siblings(".menu-item")
-    // menuItem.children(".sub-menu").removeClass("show")
-    //$(this).parent().siblings(".menu-item").children(".sub-menu").removeClass("show")
-    // console.log("this function works on menu item has children")
-  })
-  // $($(".menu-item a")).on("focusin", function () {
-  //   var topLevelMenu = $(".menu-item a").siblings().find(".menu-item-has-children")
-  //   //console.log(topLevelMenu)
-  //   console.log(topLevelMenu)
-  //   if (topLevelMenu) {
-  //     console.log("top level menu")
-  //   }
-  // })
-  // // $(".sub-menu .menu-item:last-child").on("focusout", function () {
-  // //   $(this).parent().removeClass("show")
-  // // })
-  // // $(".sub-menu li").on("focusout", function () {
-  // //   $(this).children("sub-menu").removeClass("show")
-  // //   console.log("this function also workssssssssdjflasdkghl")
-  // // })
-  // var nextMenuItem = $(".menu-item-has-children").siblings("menu-item")
-  // console.log(nextMenuItem)
-  // nextMenuItem.on("mousein", function () {
-  //   $(this).siblings(".menu-item-has-children sub-menu").removeClass("show")
-  // })
-  // $(".menu-item:not(:)")
-
+  //hide submenu on click outside it's container
   const $menu = $(".sub-menu")
 
   $(document).mouseup((e) => {
@@ -54,6 +17,35 @@
     ) {
       // ... nor a descendant of the container
       $menu.removeClass("show")
+    }
+  })
+  // //show submenu on enter keypress
+  $(".menu-item-has-children .submenu-icon").on("keypress", function (e) {
+    var keycode = e.keyCode ? e.keyCode : e.which
+    if (keycode == "13") {
+      $(this).siblings(".sub-menu").toggleClass("show")
+    }
+  })
+  // //hide submenu on reverse selection
+  $(".menu-item-has-children a").on("focusin", function () {
+    $(this).siblings(".sub-menu").removeClass("show")
+  })
+  $(".menu-item a").on("focusin", function () {
+    var menuItem = $(this).parent().siblings(".menu-item")
+    menuItem.children(".sub-menu").removeClass("show")
+  })
+
+  $(document).keyup(function (e) {
+    if (e.key === "Escape") {
+      // escape key maps to keycode `27`
+      // <DO YOUR WORK HERE>
+
+      console.log("esc key pressed")
+      if ($(".menu-item-has-children .sub-menu .menu-item-has-children .sub-menu").hasClass("show")) {
+        $(".menu-item-has-children .sub-menu .menu-item-has-children .sub-menu").removeClass("show")
+      } else {
+        $(".menu-item-has-children .sub-menu").removeClass("show")
+      }
     }
   })
 })(jQuery)
