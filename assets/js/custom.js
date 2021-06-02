0969b8b2aa06775ee776852bada8557f4f96d02f
Ashis Bajracharya <ashis.n1tech@gmail.com>
2021-06-02 15:40:17 +0545

    keyboard focus functionality for the nested-menus milestone 1

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,7 +1,28 @@
 ;(function ($) {
+  $(".menu-item-has-children .submenu-icon").attr("tabindex", "0")
   $(".menu-item-has-children .submenu-icon").on("click", function () {
     $(this).siblings(".sub-menu").toggleClass("show")
     console.log("this function works")
+  })
+
+  $(".menu-item-has-children .submenu-icon").on("keypress", function (e) {
+    console.log("this functionworks on keypress")
+    var keycode = e.keyCode ? e.keyCode : e.which
+    if (keycode == "13") {
+      $(this).siblings(".sub-menu").toggleClass("show")
+    }
+  })
+  $(".sub-menu .menu-item:last-child").on("focusout", function () {
+    $(this).parent().removeClass("show")
+  })
+  $(".sub-menu").on("focusout", function () {
+    $(this).children("sub-menu").removeClass("show")
+  })
+
+  var nextMenuItem = $(".menu-item-has-children").siblings("menu-item")
+  console.log(nextMenuItem)
+  nextMenuItem.on("mousein", function () {
+    console.log("this function works on focus")
   })
   const $menu = $(".sub-menu")
 
